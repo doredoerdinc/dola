398caf79dff45c97836382088cdfae0a70e1b30d
esimsek <erdinc.simsek@doredo.com>
2025-08-01 07:36:11 +0300

    last route calculate

--- doredo.Module.Web/Controllers/GoogleService.cs
+++ doredo.Module.Web/Controllers/GoogleService.cs
@@ -41,7 +41,7 @@ namespace dola.Module.Web
     }
     public class googleService
     {
-
+        public static string GoogleAPIKEY="AIzaSyBIMx0eWPwIW4LyicLuTUhpdWuuNq3rVz4";
         public static void CalculateRouteMatrixInChunks(List<Address> fromAddresses, List<Address> toAddresses, IObjectSpace objectSpace)
         {
             const int maxOrigins = 10;
@@ -75,9 +75,8 @@ namespace dola.Module.Web
         public static void CalculateRouteMatrix(List<Address> fromAddresses, List<Address> toAddresses, IObjectSpace objectSpace)
         {
             using (HttpClient client = new HttpClient())
-            {
-                var apiKey = "AIzaSyBIMx0eWPwIW4LyicLuTUhpdWuuNq3rVz4"; // TODO: config'e taşı
-                var url = $"https://maps.googleapis.com/maps/api/distancematrix/json?key={apiKey}";
+            { 
+                var url = $"https://maps.googleapis.com/maps/api/distancematrix/json?key={GoogleAPIKEY}";
 
                 var origins = string.Join("|", fromAddresses.Select(f =>
                     $"{f.Latitude.ToString(CultureInfo.InvariantCulture)},{f.Longitude.ToString(CultureInfo.InvariantCulture)}"));
@@ -153,6 +152,62 @@ namespace dola.Module.Web
                 }
             }
         }
+
+        public static void CalculateDirectionsMatrix(List<Address> fromAddresses, List<Address> toAddresses, IObjectSpace objectSpace)
+        {
+            var client = new HttpClient();
+
+            foreach (var from in fromAddresses)
+            {
+                foreach (var to in toAddresses)
+                {
+                    if (from.SysCode == to.SysCode)
+                        continue;
+
+                    var origin = $"{from.Latitude.ToString(CultureInfo.InvariantCulture)},{from.Longitude.ToString(CultureInfo.InvariantCulture)}";
+                    var destination = $"{to.Latitude.ToString(CultureInfo.InvariantCulture)},{to.Longitude.ToString(CultureInfo.InvariantCulture)}";
+
+                    var url = $"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&key={GoogleAPIKEY}&mode=driving";
+
+                    var response = client.GetAsync(url).Result;
+                    if (!response.IsSuccessStatusCode)
+                        continue;
+
+                    var json = response.Content.ReadAsStringAsync().Result;
+                    var directions = JsonConvert.DeserializeObject<GoogleDirectionsResponse>(json);
+
+                    var route = directions?.Routes?.FirstOrDefault();
+                    if (route == null)
+                        continue;
+
+                    var polyline = route.OverviewPolyline?.Points;
+                    var leg = route.Legs?.FirstOrDefault();
+
+                    if (polyline == null || leg == null)
+                        continue;
+
+                    // Veritabanı güncelleme
+                    var criteria = GroupOperator.Combine(
+                        GroupOperatorType.And,
+                        new BinaryOperator("FromAddress.Syscode", from.SysCode, BinaryOperatorType.Equal),
+                        new BinaryOperator("ToAddress.SysCode", to.SysCode, BinaryOperatorType.Equal));
+
+                    var admat = objectSpace.FindObject<AddressRouteMatrix>(criteria) ?? objectSpace.CreateObject<AddressRouteMatrix>();
+
+                    admat.FromAddress = objectSpace.GetObjectByKey<Address>(from.SysCode);
+                    admat.ToAddress = objectSpace.GetObjectByKey<Address>(to.SysCode);
+                    admat.EncodedPolyline = polyline;
+                    admat.Duration = leg.Duration?.Value ?? 0;
+                    admat.DictanceMeters = leg.Distance?.Value ?? 0;
+
+                    objectSpace.CommitChanges();
+
+                    // Kota aşımını önlemek için kısa bekleme koyabilirsin
+                  //  Thread.Sleep(150);
+                }
+            }
+        }
+
         public static void CalculateRouteMatrixMissing(List<Address> addressList, IObjectSpace objectSpace)
         {
             var fromAddressRequestList = new List<Address>();
@@ -183,7 +238,7 @@ namespace dola.Module.Web
             // Eksik kayıtlar varsa API çağrısı yap
             if (fromAddressRequestList.Count > 0)
             {
-                CalculateRouteMatrixInChunks(fromAddressRequestList, toAddressRequestList, objectSpace);
+                CalculateDirectionsMatrix(fromAddressRequestList, toAddressRequestList, objectSpace);
             }
         }
 
@@ -246,6 +301,64 @@ namespace dola.Module.Web
         public int Value { get; set; } // saniye cinsinden
     }
 
-  
+
+public class GoogleDirectionsResponse
+    {
+        [JsonProperty("routes")]
+        public List<Route> Routes { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public class Route
+    {
+        [JsonProperty("overview_polyline")]
+        public OverviewPolyline OverviewPolyline { get; set; }
+
+        [JsonProperty("legs")]
+        public List<Leg> Legs { get; set; }
+    }
+
+    public class OverviewPolyline
+    {
+        [JsonProperty("points")]
+        public string Points { get; set; }
+    }
+
+    public class Leg
+    {
+        [JsonProperty("distance")]
+        public Distance Distance { get; set; }
+
+        [JsonProperty("duration")]
+        public Duration Duration { get; set; }
+
+        [JsonProperty("start_address")]
+        public string StartAddress { get; set; }
+
+        [JsonProperty("end_address")]
+        public string EndAddress { get; set; }
+
+        [JsonProperty("steps")]
+        public List<Step> Steps { get; set; }
+    } 
+    public class Step
+    {
+        [JsonProperty("distance")]
+        public Distance Distance { get; set; }
+
+        [JsonProperty("duration")]
+        public Duration Duration { get; set; }
+
+        [JsonProperty("html_instructions")]
+        public string HtmlInstructions { get; set; }
+
+        [JsonProperty("polyline")]
+        public OverviewPolyline Polyline { get; set; }
+
+        [JsonProperty("travel_mode")]
+        public string TravelMode { get; set; }
+    }
 
 }
